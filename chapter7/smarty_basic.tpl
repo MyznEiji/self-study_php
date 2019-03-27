aa997a69d9ce0e8bf7290a73c899583f4bbb8eb9
MyznEiji <em711th@icloud.com>
2019-03-27 00:38:31 +0000

    hoge

---
+++ chapter7/smarty_basic.tpl
@@ -0,0 +1,11 @@
+  <!DOCTYPE html>
+  <html>
+  <head>
+  <meta charset="UTG-8" />
+  <title>Smarty</title>
+  </head>
+  <body>
+  <p>{$message}</p>
+  </body>
+
+  </html>
---
+++ chapter7/templates_c/e2688befd88ef283653674b09e9fde4111c21f20_0.file.smarty_basic.tpl.php
@@ -0,0 +1,37 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-03-27 09:35:02
+  from '/root/projects/self-study_php/chapter7/smarty_basic.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5c9ac536559e19_54397798',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e2688befd88ef283653674b09e9fde4111c21f20' => 
+    array (
+      0 => '/root/projects/self-study_php/chapter7/smarty_basic.tpl',
+      1 => 1553646724,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5c9ac536559e19_54397798 (Smarty_Internal_Template $_smarty_tpl) {
+?>  <!DOCTYPE html>
+  <html>
+  <head>
+  <meta charset="UTG-8" />
+  <title>Smarty</title>
+  </head>
+  <body>
+  <p><?php echo $_smarty_tpl->tpl_vars['message']->value;?>
+</p>
+  </body>
+
+  </html>
+<?php }
+}
